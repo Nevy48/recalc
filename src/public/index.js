eb8133846db544dd37f600db1fe59507c27d4058
santilibano7 <santilibano27@gmail.com>
2023-06-14 00:23:39 -0300

    Se agregó conexión de Frontend con API para √

--- src/public/index.js
+++ src/public/index.js
@@ -1,7 +1,7 @@
 const $display = document.querySelector('.display')
 const $buttons = document.querySelector('.buttons')
 
-const operations = ['-', '/'];
+const operations = ['-', '/','√'];
 
 let currentDisplay = "";
 let operation = null;
@@ -23,6 +23,10 @@ $buttons.addEventListener('click', async (e) => {
 
         if (operation === "/") {
             result = await calculateDiv(firstArg, secondArg)
+        }
+
+        if (operation === "√") {
+            result = await calculateSqrt(firstArg)
         }
 
         reset = true;
@@ -55,6 +59,13 @@ async function calculateSub(firstArg, secondArg) {
 
 async function calculateDiv(firstArg, secondArg) {
     const resp = await fetch(`/api/v1/div/${firstArg}/${secondArg}`)
+    const { result } = await resp.json();
+
+    return result;
+}
+
+async function calculateSqrt(firstArg) {
+    const resp = await fetch(`/api/v1/sqrt/${firstArg}`)
     const { result } = await resp.json();
 
     return result;
